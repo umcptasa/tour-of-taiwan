71c7a79e6fb4c6015203411bb50b2f998231112f
Stephan Loh <sloh4137@gmail.com>
2019-10-29 19:14:24 +0000

    Add new plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,71 @@
 module.exports = {
   pathPrefix: `/`,
   siteMetadata: {
-    title: 'Tour of Taiwan',
+    title: "Tour of Taiwan",
   },
   plugins: [
-    'gatsby-plugin-resolve-src',
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-offline',
-    'gatsby-plugin-sass',
-    'gatsby-plugin-material-ui',
-    'gatsby-plugin-flow',
+    "gatsby-plugin-resolve-src",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sass",
+    "gatsby-plugin-material-ui",
+    "gatsby-plugin-flow",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'UMCP TASA Tour of Taiwan',
-        short_name: 'Tour of Taiwan',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/assets/img/favicon.png', // This path is relative to the root of the site.
+        name: "UMCP TASA Tour of Taiwan",
+        short_name: "Tour of Taiwan",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: "src/assets/img/favicon.png", // This path is relative to the root of the site.
+      },
+    },
+    "gatsby-plugin-offline",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown`,
+        path: `${__dirname}/content/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        useMozJpeg: false,
+        stripMetadata: true,
+        defaultQuality: 75,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [
+          {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+              plugins: [`gatsby-remark-copy-linked-files`],
+            },
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+            },
+          },
+        ],
       },
     },
   ],
