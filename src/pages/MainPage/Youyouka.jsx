f6a19d89f394e71243996017d1e0ed76054ce6f5
Stephan Loh <sloh4137@gmail.com>
2019-10-31 00:18:16 +0000

    Remove template pages

--- src/pages/MainPage/Youyouka.jsx
+++ src/pages/MainPage/Youyouka.jsx
@@ -3,37 +3,31 @@
  * @flow
  */
 
-import React from "react";
+import React from "react"
 // nodejs library that concatenates classes
-import classNames from "classnames";
+import classNames from "classnames"
 // react components for routing our app without refresh
-import { Link } from "gatsby";
+import { Link } from "gatsby"
 // @material-ui/core components
-import withStyles from "@material-ui/core/styles/withStyles";
+import withStyles from "@material-ui/core/styles/withStyles"
 // @material-ui/icons
 // core components
-import MoneyVerifier from "modules/MoneyVerifier.js";
-import GridContainer from "components/Grid/GridContainer.jsx";
-import GridItem from "components/Grid/GridItem.jsx";
-import Button from "components/CustomButtons/Button.jsx";
+import MoneyVerifier from "modules/MoneyVerifier.js"
+import GridContainer from "components/Grid/GridContainer.jsx"
+import GridItem from "components/Grid/GridItem.jsx"
+import Button from "components/CustomButtons/Button.jsx"
 
 type Props = {
-    verifier: MoneyVerifier
-};
+  verifier: MoneyVerifier,
+}
 
 const Youyouka = (props: Props) => {
-    const {verifier} = props;
-    return (<div> 
-        <h1>
-            Placeholder
-        </h1>
-        <Button type="button" onPress={verifier.add(1, "chicken")}>Add</Button>
-        <Button type="button" onPress={verifier.subtract(1, "chicken")}>Subtract</Button>
-    </div>);
+  const { verifier } = props
+  return <div></div>
 }
 
 Youyouka.defaultProps = {
-    // if we had any optional props, we can define what values they have here
-};
+  // if we had any optional props, we can define what values they have here
+}
 
 export default Youyouka
