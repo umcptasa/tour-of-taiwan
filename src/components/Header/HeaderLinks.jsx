fb0dd5548f0b839838aceba5cf195dc9d1be90e1
Stephan Loh <sloh4137@gmail.com>
2019-10-31 00:16:41 +0000

    Remove components from header

--- src/components/Header/HeaderLinks.jsx
+++ src/components/Header/HeaderLinks.jsx
@@ -26,29 +26,6 @@ function HeaderLinks({ ...props }) {
   return (
     <List className={classes.list}>
       <ListItem className={classes.listItem}>
-        <CustomDropdown
-          noLiPadding
-          buttonText="Components"
-          buttonProps={{
-            className: classes.navLink,
-            color: "transparent",
-          }}
-          buttonIcon={Apps}
-          dropdownList={[
-            <Link to="/" className={classes.dropdownLink}>
-              All components
-            </Link>,
-            <a
-              href="https://creativetimofficial.github.io/material-kit-react/#/documentation"
-              target="_blank"
-              className={classes.dropdownLink}
-            >
-              Documentation
-            </a>,
-          ]}
-        />
-      </ListItem>
-      <ListItem className={classes.listItem}>
         <Link to="/schedule-page" className={classes.navLink}>
           <Schedule className={classes.icons} />
           Schedule
