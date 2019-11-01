bcde756e3075d0c7703c3b4c1fc2159b60dfe985
Stephan Loh <sloh4137@gmail.com>
2019-11-01 01:40:52 +0000

    Prepare for sending name to components

--- src/pages/SchedulePage/Schedule.jsx
+++ src/pages/SchedulePage/Schedule.jsx
@@ -5,19 +5,13 @@
 
 import React from "react"
 // nodejs library that concatenates classes
-import classNames from "classnames"
+//import classNames from "classnames"
 // Gatsby GraphQL libraries
 import { useStaticQuery, graphql } from "gatsby"
 // @material-ui/core components
-import withStyles from "@material-ui/core/styles/withStyles"
+//import withStyles from "@material-ui/core/styles/withStyles"
 // @material-ui/icons
 // core components
-import materialStyle from "assets/jss/material-kit-react.jsx"
-import Table from "@material-ui/core/Table"
-import TableBody from "@material-ui/core/TableBody"
-import TableCell from "@material-ui/core/TableCell"
-import TableHead from "@material-ui/core/TableHead"
-import TableRow from "@material-ui/core/TableRow"
 
 type Props = {}
 
--- src/pages/SchedulePage/SchedulePage.jsx
+++ src/pages/SchedulePage/SchedulePage.jsx
@@ -7,19 +7,13 @@ import React from "react";
 // nodejs library that concatenates classes
 import classNames from "classnames";
 // react components for routing our app without refresh
-import { Link } from "gatsby";
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
 // @material-ui/icons
 // core components
-import MoneyVerifier from "modules/MoneyVerifier.js";
 import Header from "components/Header/Header.jsx";
 import HeaderLinks from "components/Header/HeaderLinks.jsx"
 import Footer from "components/Footer/Footer.jsx";
-import GridContainer from "components/Grid/GridContainer.jsx";
-import GridItem from "components/Grid/GridItem.jsx";
-import Button from "components/CustomButtons/Button.jsx";
-import Parallax from "components/Parallax/Parallax.jsx";
 
 type Props = {};
 
