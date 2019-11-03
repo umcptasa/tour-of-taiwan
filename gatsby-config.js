f64dcff8fc4273585109edfe909139c0912a1208
Stephan Loh <sloh4137@gmail.com>
2019-11-03 19:48:22 +0000

    Add sharp transformer plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,72 +1,73 @@
 module.exports = {
-  pathPrefix: `/tour-of-taiwan`,
-  siteMetadata: {
-    title: "Tour of Taiwan",
-  },
-  plugins: [
-    "gatsby-plugin-resolve-src",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sass",
-    "gatsby-plugin-material-ui",
-    "gatsby-plugin-flow",
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: "UMCP TASA Tour of Taiwan",
-        short_name: "Tour of Taiwan",
-        start_url: "/tour-of-taiwan",
-        background_color: "#663399",
-        theme_color: "#663399",
-        display: "standalone",
-        icon: "static/youyouka-512.png", // This path is relative to the root of the site.
-      },
+    pathPrefix: `/tour-of-taiwan`,
+    siteMetadata: {
+        title: "Tour of Taiwan",
     },
-    "gatsby-plugin-offline",
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `markdown`,
-        path: `${__dirname}/content/`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-sharp`,
-      options: {
-        useMozJpeg: false,
-        stripMetadata: true,
-        defaultQuality: 75,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        // CommonMark mode (default: true)
-        commonmark: true,
-        // Footnotes mode (default: true)
-        footnotes: true,
-        // Pedantic mode (default: true)
-        pedantic: true,
-        // GitHub Flavored Markdown mode (default: true)
-        gfm: true,
-        // Plugins configs
-        plugins: [
-          {
+    plugins: [
+        "gatsby-plugin-resolve-src",
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sass",
+        "gatsby-plugin-material-ui",
+        "gatsby-plugin-flow",
+        "gatsby-plugin-sharp", 
+        "gatsby-transformer-sharp",
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `markdown`,
+                path: `${__dirname}/content/`,
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/assets/img/`,
+            },
+        },
+        {
             resolve: `gatsby-transformer-remark`,
             options: {
-              plugins: [`gatsby-remark-copy-linked-files`],
+                // CommonMark mode (default: true)
+                commonmark: true,
+                // Footnotes mode (default: true)
+                footnotes: true,
+                // Pedantic mode (default: true)
+                pedantic: true,
+                // GitHub Flavored Markdown mode (default: true)
+                gfm: true,
+                // Plugins configs
+                plugins: [
+                    {
+                        resolve: `gatsby-transformer-remark`,
+                        options: {
+                            plugins: [`gatsby-remark-copy-linked-files`],
+                        },
+                    },
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            // It's important to specify the maxWidth (in pixels) of
+                            // the content container as this plugin uses this as the
+                            // base for generating different widths of each image.
+                            maxWidth: 590,
+                        },
+                    },
+                ],
             },
-          },
-          {
-            resolve: `gatsby-remark-images`,
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
             options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 590,
+                name: "UMCP TASA Tour of Taiwan",
+                short_name: "Tour of Taiwan",
+                start_url: "/tour-of-taiwan",
+                background_color: "#663399",
+                theme_color: "#663399",
+                display: "standalone",
+                icon: "static/youyouka-512.png", // This path is relative to the root of the site.
             },
-          },
-        ],
-      },
-    },
-  ],
-}
+        },
+        "gatsby-plugin-offline",
+    ],
+};
